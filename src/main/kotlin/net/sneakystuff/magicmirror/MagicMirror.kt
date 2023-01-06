e4fd25528cad406836c653021bd3ca02696b07b5
Brandon Simmons <bsimmons@ycp.edu>
2023-01-06 18:59:28 -0500

    adding test item

--- src/main/kotlin/net/sneakystuff/magicmirror/MagicMirror.kt
+++ src/main/kotlin/net/sneakystuff/magicmirror/MagicMirror.kt
@@ -1,9 +1,16 @@
 package net.sneakystuff.magicmirror
 
 import net.fabricmc.api.ModInitializer
+import net.sneakystuff.magicmirror.registry.ModItems
 
-class MagicMirror : ModInitializer {
+class MagicMirror() : ModInitializer {
+
     override fun onInitialize() {
+        val modItems = ModItems()
+        modItems.registerItems()
+    }
 
+    companion object {
+        const val MOD_ID: String = "mmirror"
     }
 }
